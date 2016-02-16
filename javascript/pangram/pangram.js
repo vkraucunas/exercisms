99ffd47a15adf030daee8e05a28a231b732c0a36
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-02-15 18:35:19 -0700

    pangram complete

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,91 @@
+module.exports = Pangram;
+
+function Pangram(string) {
+    this.isPangram = function () {
+        if (string.length < 26) {
+            return false;
+        } else {
+            return checking(string);
+        }
+    }
+}
+
+function checking(str) {
+    str = str.toLowerCase();
+    var alphabet = [ 'a',
+          'b',
+          'c',
+          'd',
+          'e',
+          'f',
+          'g',
+          'h',
+          'i',
+          'j',
+          'k',
+          'l',
+          'm',
+          'n',
+          'o',
+          'p',
+          'q',
+          'r',
+          's',
+          't',
+          'u',
+          'v',
+          'w',
+          'x',
+          'y',
+          'z' ];
+    var result = alphabet.map(function (current, index) {
+        return str.includes(current)? true : false;
+    });
+
+    return result.indexOf(false) === -1;
+}
+    //first solution
+//     var alphabet = {
+//         a: false,
+//         b: false,
+//         c: false,
+//         d: false,
+//         e: false,
+//         f: false,
+//         g: false,
+//         h: false,
+//         i: false,
+//         j: false,
+//         k: false,
+//         l: false,
+//         m: false,
+//         n: false,
+//         o: false,
+//         p: false,
+//         q: false,
+//         r: false,
+//         s: false,
+//         t: false,
+//         u: false,
+//         v: false,
+//         w: false,
+//         x: false,
+//         y: false,
+//         z: false
+//     };
+//     str = str.split('');
+//     for (var i = 0; i <str.length; i++) {
+//         //console.log(str[i]);
+//         if(!alphabet[str[i]]) {
+//             alphabet[str[i].toLowerCase()] = true;
+//         }
+//     }
+//     //console.log(alphabet);
+//     // console.log("updated alphabet", alphabet);
+//     for (var i=0 in alphabet) {
+//         if (!alphabet[i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
